5abbff8df7755dec6575de071d6bcea481f97155
Igor Kharchenko <hatand@bk.ru>
2022-12-12 01:38:43 +0700

    Добавление тегов для подгрузки репозиториев и Entity

--- src/main/java/com/vsk/clinic_backend/ClinicBackendApplication.java
+++ src/main/java/com/vsk/clinic_backend/ClinicBackendApplication.java
@@ -2,12 +2,15 @@ package com.vsk.clinic_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.vsk.clinic_backend.repository")
+@EntityScan(basePackages = "com.vsk.clinic_backend.entity")
 public class ClinicBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClinicBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicBackendApplication.class, args);
+    }
 }
